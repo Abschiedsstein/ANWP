0a153de1964b97b3c0798100cc03b5081d233cf5
Henrik tom Wörden <henrik@trineo.org>
2014-12-25 12:56:51 +0100

    collecting Ideas for structure

---
+++ draft.py
@@ -0,0 +1,81 @@
+class RnD : Server{
+        draw()
+        reveal(n)
+        shuffle()
+        pick()
+        }
+
+class Archive : Server{
+        add(face)
+        reveal(n)
+        pick()
+        }
+
+class Hand{
+    add()
+    remove()
+    reveal()
+    }
+
+class Server {
+        create()
+        install()
+        }
+
+class HQ : Server{
+        }
+
+class Remote : Server {
+        }
+
+class Rule : EventReader{
+        target,
+        checkConditions(),
+        effect()
+        neffect()
+        }
+
+class Blocker :Rule {
+        }
+
+class Player {
+        turn,
+        clicks,
+        credits,
+        }
+
+class Event {
+        registerAsEventReader()
+        readersList
+        }
+
+class TimeEvent : Event {
+        def init():
+            super(Event,self)
+        }
+
+class PlayerActionEvent : Event {
+        }
+
+class EventReader {
+        def triggerFunction ()
+        Event.registerAsEventReader(triggerFunction)
+        }
+
+class EventHandler {
+        }
+
+
+def subjectToRules (func):
+    def rulesChecker(*args, **kwargs):
+        relevantRules = [rule in rules if rule.target is func]
+        for rule in relevantRules:
+            if rule.checkConditions(*args,**kwargs):
+                if ruel is Blocker:
+                    return
+                rule.effect()
+            else: 
+                rule.neffect()
+        return func(*args,**kwargs)
+    return rulesChecker
+
